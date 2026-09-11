71256c72bf6ddbf804a69388f341fd45c61bc6ce
25ee052-ai <25ee052@rmkec.ac.in>
2026-09-11 14:32:23 +0530

    Add files via upload

---
+++ MEX Multiset cf.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void solve(){
+	int N; cin >> N;
+	vector<int> arr(N);
+	vector<int> zi;
+	for(int i=0; i<N; i++){
+		cin >> arr[i];
+		if(arr[i] == 0) zi.push_back(i);
+	}
+	if(zi.size() == 1){
+		cout << "NO" << endl;
+		return ;
+	}
+	string s(N, 'C');
+	if(zi.size()>0){
+		int fzi = zi[0];
+		s[fzi] = 'B';
+		for(unsigned int i=1; i<zi.size(); i++){
+			s[zi[i]] = 'A';
+		}
+	}
+	cout << "YES" << endl << s << endl;
+}
+int main(){
+	int T; cin >> T;
+	while(T--){
+		solve();
+	}
+}
